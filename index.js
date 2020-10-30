954d6a12c3fd19c3de2c19859471dbc2e2f5c54c
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-30 16:13:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Formatter {
   }
 
   static sanitize(string) {
-    const regex = "[^A-Za-z0-9\s\-\']+/g"
+    const regex = /[^A-Za-z0-9\s\-\']+/g
     return string.replace(regex, '')
   }
 }
