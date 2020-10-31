e4219b952df6ff8230c81b5e18b76a55e791bf34
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-31 03:37:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ class Formatter {
     //   }
     // }
 
-    array.forEach((word, index) => {
-      if(index > 0 && this.findWord(word)) {
-        titleizeString.push(word);
-      }else{
-        titleizeString.push(this.capitalize(word));
-      }
-    })
+    // array.forEach((word, index) => {
+    //   if(index > 0 && this.findWord(word)) {
+    //     titleizeString.push(word);
+    //   }else{
+    //     titleizeString.push(this.capitalize(word));
+    //   }
+    // })
 
     for (let i = 0; i < array.length; i++) {
       if(i > 0 && this.findWord(word)) {
